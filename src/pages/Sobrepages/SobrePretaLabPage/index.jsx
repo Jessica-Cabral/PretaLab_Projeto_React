8f2a41a6be1c70089a5419f6c7eb24c902950013
Jessica Cabral <j.pires.cursos@gmail.com>
2024-02-16 12:32:51 -0300

    Atualizando projeto

--- src/pages/Sobrepages/SobrePretaLabPage/index.jsx
+++ src/pages/Sobrepages/SobrePretaLabPage/index.jsx
@@ -1,5 +1,24 @@
+import { Stack } from "@mui/material";
+import Typography from "@mui/material/Typography";
+
 const SobrePretaLabPage = () => {
-    return <h1>Sobre PretaLab page</h1>;
-  };
-  
-  export default SobrePretaLabPage;
+  return (
+    <main>
+      <section>
+        <Stack mt="100px" alignItems="center" >
+          <Typography variant="h1" sx={{mt: 1, mb: 5, alignItems:"center", color: "#be89cb"}}>PretaLab</Typography>
+          <Typography variant="p" sx={{mt: 1, mb: 5, alignItems:"center"}}>A PretaLab é uma iniciativa que reforça a necessidade e a pertinência de incluir mais mulheres negras na inovação e na tecnologia. <span>Plataforma que conecta mulheres negras que são ou gostariam de ser da tecnologia, por meio dos nossos ciclos formativos, rede de profissionais, mercado de trabalho, consultorias e estudos.</span></Typography>
+        </Stack>
+      </section>
+      <section>
+        <Stack>
+          <Typography variant="h2" sx={{mt: 5, mb:10, alignItems:"center", color: "#be89cb"}}>Turma 2 | Ciclo Formativo Básico em Tecnológia</Typography>
+          <img src="/pretalab.jpg"/>
+        </Stack>
+      
+      </section>
+    </main>
+  );
+}
+    
+export default SobrePretaLabPage;
